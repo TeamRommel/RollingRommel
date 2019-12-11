9d8d4095429fbda6efa3c2a1499fb0c349289174
Sami1711 <sami.vanttinen@student.hamk.fi>
2019-12-11 17:38:27 +0200

    MenuUpdaed

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -1,6 +1,12 @@
 extends Control
 
-
 func _on_TextureButton_pressed():
-	get_tree().change_scene("res://scenes/GameScene.tscn")
-	
+	get_tree().change_scene("res://scenes/GameScene.tscn")	
+
+func _on_SingleGame_pressed():
+	$TextureRect/SingleRing.visible = true
+	$TextureRect/DesertRing.visible = false
+
+func _on_DesertCup_pressed():
+	$TextureRect/SingleRing.visible = false
+	$TextureRect/DesertRing.visible = true
