9fcf30e0e943fd88807ce658aaa78fc864734e7d
juhario <juha.rajala@student.hamk.fi>
2019-12-12 18:04:17 +0200

    New track added

--- scripts/GameSettings.gd
+++ scripts/GameSettings.gd
@@ -2,7 +2,7 @@ extends Node
 
 var no_of_players: int = 0
 var no_of_humans: int = 0
-var current_level: int = 3
+var current_level: int = 4
 var players: Array = []
 var player_class = preload("PlayerData.gd")
 var levels: Array = []
@@ -11,6 +11,7 @@ var track_1 = preload("res://scenes/tracks/track_1.tscn")
 var track_2 = preload("res://scenes/tracks/track_2.tscn")
 var track_3 = preload("res://scenes/tracks/track_3.tscn")
 var track_4 = preload("res://scenes/tracks/track_4.tscn")
+var track_5 = preload("res://scenes/tracks/track_5.tscn")
 var player_car = preload("res://scenes/PlayerVehicle.tscn")
 var cpu_car = preload("res://scenes/CPUVehicle.tscn")
 
@@ -22,6 +23,7 @@ func create_level_array() -> void:
 	levels.append(track_2)
 	levels.append(track_3)
 	levels.append(track_4)
+	levels.append(track_5)
 
 func _init():
 	var p1 = player_class.new("Monty", 0, false)
