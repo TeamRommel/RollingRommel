e21175e3905b1ac46dbbd937406764450f253eb1
juhario <juha.rajala@student.hamk.fi>
2019-12-13 11:10:23 +0200

    Added option to skip intro (spacebar)

--- scripts/VideoPlayer.gd
+++ scripts/VideoPlayer.gd
@@ -1,16 +1,15 @@
 extends VideoPlayer
 
-var video_length = 34.4
+var video_length = 34.5
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
+
+# Check if video is at the end or spacebar button is pressed
+# If either is true then change scene to Main menu
 func _process(delta):
-	if get_stream_position() >= video_length:
-		get_parent().queue_free()
+	if (get_stream_position() >= video_length) or (Input.is_action_pressed('action_1_1')):
 		get_tree().change_scene("res://scenes/MainMenu.tscn")
 
