b661438086bbe9e81c517616f78c7e9175cc9124
Sami1711 <sami.vanttinen@student.hamk.fi>
2019-12-18 20:52:41 +0200

    menu update

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -10,3 +10,25 @@ func _on_DesertCup_pressed():
 	$TextureRect/SingleRing.visible = false
 	$TextureRect/DesertRing.visible = true
 
+func _on_playersButton1_pressed():
+	$TextureRect/players1Ring.visible = true
+	$TextureRect/players2Ring.visible = false
+
+func _on_playersButton2_pressed():
+	$TextureRect/players1Ring.visible = false
+	$TextureRect/players2Ring.visible = true
+
+func _on_difficulty1_pressed():
+	$TextureRect/difficulty1Ring.visible = true
+	$TextureRect/difficulty2Ring.visible = false
+	$TextureRect/difficulty3Ring.visible = false
+
+func _on_difficulty2_pressed():
+	$TextureRect/difficulty1Ring.visible = false
+	$TextureRect/difficulty2Ring.visible = true
+	$TextureRect/difficulty3Ring.visible = false
+
+func _on_difficulty3_pressed():
+	$TextureRect/difficulty1Ring.visible = false
+	$TextureRect/difficulty2Ring.visible = false
+	$TextureRect/difficulty3Ring.visible = true
